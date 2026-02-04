da21f4f108759b18f5eba839be3ffe3745d983e7
PrinceKaklotar <princekaklotar5@gmail.com>
2026-02-04 22:42:44 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+// import { Home } from 'lucide-react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProject from './pages/MyProject'
+import Preview from './pages/Preview'
+import Community from './pages/Community'
+import View from './pages/View'
+import Home from './pages/Home'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <div>
+       <Navbar/>
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/pricing' element={<Pricing/>} />
+          <Route path='/projects/:projectId' element={<Projects/>} />
+          <Route path='/projects' element={<MyProject/>} />
+          <Route path='/preview/:projectId' element={<Preview/>} />
+          <Route path='/preview/:projectId/:versionId' element={<Preview/>} />
+          <Route path='/community' element={<Community/>} />
+          <Route path='/view/:projectId' element={<View/>} />
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
